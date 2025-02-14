666694ee98461f877be01d06d799ee6c84265d6e
xxl4 <nice.lizhi@gmail.com>
2025-02-14 09:06:57 +0000

    fix

---
+++ src/Http/Controllers/Web/FeedsController.php
@@ -0,0 +1,65 @@
+<?php
+namespace NexaMerchant\Feeds\Http\Controllers\Web;
+
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Spatie\Feed\Feed;
+use Spatie\Feed\Helpers\ResolveFeedItems;
+use Webkul\Product\Repositories\ProductRepository;
+use Webkul\Product\Models\Product;
+
+
+class FeedsController extends Controller
+{
+    public function __construct(
+        protected ProductRepository $productRepository
+    )
+    {
+        
+    }
+    // generate by json for klaviyo
+    public function atom(Request $request)
+    {
+        // 
+        $products = $this->productRepository->all();
+
+
+    }
+
+    /**
+     * 
+     * @param Request $request
+     * 
+     * 
+     * @link https://github.com/klaviyo/devportal/blob/master/custom_catalog_example.json
+     * @return \Illuminate\Http\JsonResponse
+     * 
+     */
+    public function klaviyo(Request $request)
+    {
+        //
+        $products = Product::where('type', 'configurable')->orderBy("updated_at","desc")->limit(10)->get();
+        $items = [];
+        foreach($products as $key => $product) {
+            $image_url = $product->images->first() ? $product->images->first()->url : '';
+            $image_url = str_replace(config('app.url').'/storage/'.config('app.url').'/storage/', config('app.url') . '/storage/', $image_url);
+            $item = [
+                'id' => $product->id,
+                'title' => $product->name,
+                'description' => $product->description,
+                'link' => config('services.shop.url') .'/products/'. $product->url_key,
+                'image_link' => $image_url,
+                'price' => $product->price,
+                'availability' => $product->isSaleable() ? 'in stock' : 'out of stock'
+            ];
+            $items[] = $item;
+        }
+
+        return response()->json($items);
+    }
+
+    
+
+    
+}
--- src/Routes/web.php
+++ src/Routes/web.php
@@ -9,12 +9,20 @@
  */
 use Illuminate\Support\Facades\Route;
 use NexaMerchant\Feeds\Http\Controllers\Web\ExampleController;
+use NexaMerchant\Feeds\Http\Controllers\Web\FeedsController;
 
 Route::group(['middleware' => ['locale', 'theme', 'currency'], 'prefix'=>'feeds'], function () {
 
     Route::controller(ExampleController::class)->prefix('example')->group(function () {
 
         Route::get('demo', 'demo')->name('feeds.web.example.demo');
+
+    });
+
+    Route::controller(FeedsController::class)->prefix('')->group(function () {
+
+        Route::get('atom', 'atom')->name('feeds.web.feeds.atom');
+        Route::get('klaviyo', 'klaviyo')->name('feeds.web.feeds.klaviyo');
 
     });
 
